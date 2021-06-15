7e1622a39378db8f069c35c0621b8103d963e1ec
yashpurwar1 <yashpurwar.yashpurwar.com>
2021-06-15 20:57:20 +0530

    Flipping coin multiple time to get winner

--- flip_coin_simulator.sh
+++ flip_coin_simulator.sh
@@ -1,11 +1,29 @@
 #!/bin/bash -x
 echo "Welcome to Flip coin simulation problem"
 
-toss=$((RANDOM%2))
+heads_count=0
+tails_count=0
+flips=0
+
+while [ $flips -ne 5 ]   #Tossing the coin for 5 times
+do
+	(( flips++ ))
+	toss=$((RANDOM%2))
 
 if [ $toss -eq 0 ]
 then
 	echo "Heads"
+	(( heads_count++ ))
 else
 	echo "Tails"
+	(( tails_count++))
+fi
+done
+echo "Heads-count=$heads_count and Tails-count=$tails_count"
+
+if [ $heads_count -gt $tails_count ]
+then
+	echo "Winner is heads"
+else
+	echo "Winner is tails"
 fi
